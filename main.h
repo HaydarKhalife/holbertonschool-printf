efdc9ab04a4a8ee911f82eb4171fea1e283632c8
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2023-11-27 00:12:28 +0200

    Updateeee main.h

--- main.h
+++ main.h
@@ -7,15 +7,18 @@
 #include <limits.h>
 #include <unistd.h>
 
+
 /**
- * struct format - match the conversion specifiers to args passed
- * @id: pointer to format specifier, i.e. id[0] = % and id[1] is the letter (c, s, d, i)
- * @f: pointer to function determined by the conversion specifier
- */
+* struct format - match the conversion specifiers to args passed
+* @id: pointer to format specifer, i.e. id[0] = % and id[1] is the letter (c, s, d, i)
+* @f: pointer to function determined by the conversion specifier
+*
+*/
+
 typedef struct format
 {
-    char *id;
-    int (*f)();
+char *id;
+int (*f)();
 } convert_match;
 
 int printf_int(va_list args);
@@ -29,4 +32,4 @@ int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
-#endif /* MAIN_H */
+#endif
